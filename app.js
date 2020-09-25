9966902b999c6a0ddaff6f329e8906fc79840ef8
KishoreJohnsan <kishorejohnsan.s@gmail.com>
2020-09-25 23:27:28 +0530

    :construction: minor changes

--- app.js
+++ app.js
@@ -1,41 +1,55 @@
-header = document.getElementById("header");
+let header = document.getElementById('header');
+const mailLink = document.getElementById('mailLink');
+const notification = document.getElementById('notification');
 
-var myScrollFunc = function() {
-  var y = window.scrollY;
+const myScrollFunc = function () {
+  let y = window.scrollY;
   if (y >= 60) {
-    header.classList.add("border-b-2");
-    header.classList.add("border-primary");
-  }
-  else if(y <= 60){
-    header.classList.remove("border-b-2");
-    header.classList.remove("border-primary");
+    header.classList.add('border-b-2');
+    header.classList.add('border-primary');
+  } else if (y <= 60) {
+    header.classList.remove('border-b-2');
+    header.classList.remove('border-primary');
   }
 };
 
-window.addEventListener("scroll", myScrollFunc);
+window.addEventListener('scroll', myScrollFunc);
 
-var d = document.documentElement,
+let d = document.documentElement,
   t = document.querySelectorAll('.theme-btn')[0],
   m = localStorage.getItem('theme');
 
 if (m == 'dark') {
   d.classList.add('theme-dark');
-  t.textContent='Dark';
+  t.textContent = 'Dark';
+} else {
+  t.textContent = 'Light';
 }
-else{
-  t.textContent='Light';
-}
-
 
 t.addEventListener('click', function () {
   if (d.classList.contains('theme-dark')) {
     d.classList.remove('theme-dark');
-    t.textContent='Light';
+    t.textContent = 'Light';
     localStorage.removeItem('theme');
   } else {
     d.classList.add('theme-dark');
     localStorage.setItem('theme', 'dark');
-    t.textContent='Dark';
+    t.textContent = 'Dark';
   }
 });
 
+mailLink.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  navigator.clipboard.writeText('kishorejohnsan.s@gmail.com').then(
+    function () {
+      notification.classList.remove('alert');
+      setTimeout(() => {
+        notification.classList.add('alert');
+      }, 1000);
+    },
+    function () {
+      alert('Error in copying Mail ID');
+    }
+  );
+});
